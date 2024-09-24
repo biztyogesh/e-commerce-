35ac2b3a2acded4b920804323f8b42d6b4e8ca8a
biztyogesh <yogesh.dhiman@biztecno.net>
2024-09-24 17:28:57 +0530

    updated css

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -5,21 +5,22 @@ import star_dull_icon from "../Assets/star_dull_icon.png";
 import Product from "../../Pages/Product";
 
 const ProductDisplay = (props) => {
+  const { product } = props;
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
-          <img src={Product.image} alt="" />
-          <img src={Product.image} alt="" />
-          <img src={Product.image} alt="" />
-          <img src={Product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
         </div>
         <div className="productdisplay-img">
-          <img className="productdisplay-main-img" src={Product.image} alt="" />
+          <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
       </div>
       <div className="productdisplay-right">
-        <h1>{Product.name}</h1>
+        <h1>{product.name}</h1>
         <div className="productdisplay-right-star">
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
@@ -32,10 +33,10 @@ const ProductDisplay = (props) => {
         <div className="productdisplay-right-prices">
           <div className="productdisplay-right-price-old">
             {" "}
-            ${Product.old_prices}
+            ${product.old_prices}
           </div>
           <div className="productdisplay-right-price-new">
-            ${Product.new_prices}
+            ${product.new_prices}
           </div>
         </div>
         <div className="productdisplay-right-description">
@@ -43,15 +44,17 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
-          <div className="productdisplay-right-size"></div>
-          <div>S</div>
-          <div>M</div>
-          <div>L</div>
-          <div>XL</div>
-          <div>XXL</div>
+          <div className="productdisplay-right-size">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
         </div>
+        <button>ADD TO CART</button>
       </div>
-      <button>ADD TO CART</button>
+
       <p className="productdisplay-right-category">
         <span>Category: </span>Women , TShirt crop Top
       </p>
